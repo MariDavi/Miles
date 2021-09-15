da1ea4f0dae0d1429c1af791205ac6d3b3c4c269
Marina Davidchuk <daseydee@gmail.com>
2021-09-15 15:22:14 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        long cost = 30_000;
+        long rate = 20;
+        long bonus = cost /rate;
+        System.out.println(bonus);
+    }
+}
